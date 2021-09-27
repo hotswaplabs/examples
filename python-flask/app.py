5a68c76dcf4b77b9ef8ca7c4e749c61f1211552d
jgoel <jay@hotswap.app>
2021-09-26 20:21:31 -0400

    Update naming from session_id to session token

--- python-flask/app.py
+++ python-flask/app.py
@@ -30,13 +30,13 @@ def index():
         data=data
     ).json()
 
-    session_id = rc['session_id']
+    token = rc['token']
 
-    # Note: You may want to save this session ID so that you can 
+    # Note: You may want to save this session token so that you can 
     #       re-instantiate the widget later when the user returns
 
-    # Pass the session_id to the frontend to render the widget
-    return flask.render_template('index.html', session_id=session_id)
+    # Pass the session token to the frontend to render the widget
+    return flask.render_template('index.html', token=token)
 
 if __name__ == '__main__':
     app.run(debug=True)
